9e9eb7d562a78908aac84a58402097dd11d221d7
Eesa Zahed <eesa.zahed@gmail.com>
2026-01-26 17:52:54 -0600

    add setup script

---
+++ setup.sh
@@ -0,0 +1,29 @@
+echo "Starting process..."
+
+if [ "$#" -ne 1 ]; then
+  echo "Parameter required PORT"
+  exit 1
+fi
+
+PORT="$1"
+
+if ! [[ "$PORT" =~ ^[0-9]+$ ]]; then
+  echo "PORT must be a non-negative integer."
+  exit 1
+fi
+
+PIDS=$(timeout 2s lsof -ti ":$PORT")
+if [ -n "$PIDS" ]; then
+  kill -9 $PIDS
+fi
+
+if ! git pull; then
+  echo "git pull failed"
+  exit 1
+fi
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+gunicorn -b ":$PORT" app:app
